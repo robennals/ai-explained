661f31164c4b93983247151af139c8c5a7d8fd75
Rob Ennals <rob@ennals.org>
2026-04-25 13:42:10 -0700

    scaffold TransformerOverview widget

---
+++ src/components/widgets/transformers/TransformerOverview/TransformerOverview.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { WidgetContainer } from "@/components/widgets/shared/WidgetContainer";
+
+export function TransformerOverview() {
+  return (
+    <WidgetContainer title="A Transformer At a Glance">
+      <div className="p-6 text-sm text-muted">Loading…</div>
+    </WidgetContainer>
+  );
+}
---
+++ src/components/widgets/transformers/TransformerOverview/index.ts
@@ -0,0 +1,1 @@
+export { TransformerOverview } from "./TransformerOverview";
